954732f6098d7aaf0e4cafac1afefe456207d583
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-03-30 02:25:37 -0300

    Theme light and dark

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -5,14 +5,14 @@ import pizzaBackground from '../../public/background.jpg' // Ajuste o caminho co
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2 antialiased">
+    <div className="grid min-h-screen grid-cols-1 antialiased md:grid-cols-2">
       <div
         style={{
           backgroundImage: `url(${pizzaBackground})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
-        className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground"
+        className="flex h-full flex-col justify-between p-10"
       >
         <div className="flex items-center gap-3 text-lg text-primary">
           <Pizza className="h-5 w-5" />
