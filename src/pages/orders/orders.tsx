88f5b15a74498b40f5d31898302bfe06a32e2341
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-03-30 23:47:22 -0300

    Adicionando pagina de Pedidos

---
+++ src/pages/orders/orders.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Input } from '../../components/ui/input'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form className="flex items-center gap-2 " action="">
+          <span className="text-sm font-semibold">Filtros</span>
+          <Input
+            type="text"
+            placeholder="Nome do Cliente"
+            className="h-8 w-[320px]"
+          />
+        </form>
+      </div>
+    </>
+  )
+}
