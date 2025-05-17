edd3487a5b50df2563ece2c13d030f890dc9942d
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-05-16 21:02:22 -0300

    Feat: Incluindo os filtros na página de pedidos

--- front-end/src/components/pagination.tsx
+++ front-end/src/components/pagination.tsx
@@ -34,37 +34,37 @@ export function Pagination({
         </div>
         <div className="flex items-center gap-2">
           <Button
+            onClick={() => onPageChange(0)}
             variant="outline"
             className="h-8 w-8 p-0"
-            onClick={() => onPageChange(0)}
             disabled={pageIndex === 0}
           >
             <ChevronsLeft className="h-4 w-4" />
             <span className="sr-only">Primeira página</span>
           </Button>
           <Button
+            onClick={() => onPageChange(pageIndex - 1)}
             variant="outline"
             className="h-8 w-8 p-0"
-            onClick={() => onPageChange(pageIndex - 1)}
             disabled={pageIndex === 0}
           >
             <ChevronLeft className="h-4 w-4" />
             <span className="sr-only">Página anterior</span>
           </Button>
           <Button
+            onClick={() => onPageChange(pageIndex + 1)}
             variant="outline"
             className="h-8 w-8 p-0"
-            onClick={() => onPageChange(pageIndex + 1)}
-            disabled={pageIndex === pages - 1}
+            disabled={pages <= pageIndex + 1}
           >
             <ChevronRight className="h-4 w-4" />
             <span className="sr-only">Próxima página</span>
           </Button>
           <Button
+            onClick={() => onPageChange(pages - 1)}
             variant="outline"
             className="h-8 w-8 p-0"
-            onClick={() => onPageChange(pages - 1)}
-            disabled={pageIndex === pages - 1}
+            disabled={pages <= pageIndex + 1}
           >
             <ChevronsRight className="h-4 w-4" />
             <span className="sr-only">Última página</span>
