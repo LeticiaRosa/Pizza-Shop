5bc919cca53a92bef4cb315481ab8f54a2bf403a
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-06-08 23:32:17 -0300

    Feat: Funcionalidade de Cancelamento de Pedido

--- front-end/src/api/cancel-order.ts
+++ front-end/src/api/cancel-order.ts
@@ -4,6 +4,6 @@ export interface CancelOrderParams {
   orderId: string
 }
 
-export async function cancelOrder(orderId: string) {
+export async function cancelOrder({ orderId }: CancelOrderParams) {
   await api.patch(`/orders/${orderId}/cancel`)
 }
--- front-end/src/pages/app/orders/order-table-row.tsx
+++ front-end/src/pages/app/orders/order-table-row.tsx
@@ -1,8 +1,11 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { Search, ThumbsUp, X } from 'lucide-react'
 import { useState } from 'react'
 
+import { cancelOrder } from '../../../api/cancel-order'
+import { GetOrdersResponse } from '../../../api/get-orders'
 import { OrderStatus } from '../../../components/order-status'
 import { Button } from '../../../components/ui/button'
 import { Dialog, DialogTrigger } from '../../../components/ui/dialog'
@@ -20,6 +23,35 @@ export interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  const { mutateAsync: cancelOrderFn } = useMutation({
+    mutationFn: cancelOrder,
+    async onSuccess(_, { orderId }) {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ['orders'],
+      })
+
+      ordersListCache.forEach(([cacheKey, cacheData]) => {
+        if (!cacheData) {
+          return
+        }
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return { ...order, status: 'canceled' }
+            }
+            return order
+          }),
+        })
+      })
+    },
+    onError(error) {
+      console.error('Erro ao cancelar o pedido:', error, order.orderId)
+    },
+  })
+
   return (
     <TableRow key={order.orderId}>
       <TableCell>
@@ -64,6 +96,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
           variant="link"
           size="sm"
           disabled={!['pending', 'processing'].includes(order.status)}
+          onClick={() => cancelOrderFn({ orderId: order.orderId })}
         >
           <X className="mr-2 h-4 w-4" />
           Cancelar
